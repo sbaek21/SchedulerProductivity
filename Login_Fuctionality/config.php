6763d7106caa3ccb7620cecf365d15c1638f7000
choiyw109 <choiyw109@gmail.com>
2024-10-12 17:32:01 -0500

    Added Comments, Deleted session.php
    Added comments into the codes for readability
    changed welcome.php to just contain session.php code and deleted session.php file.

--- Login_Fuctionality/config.php
+++ Login_Fuctionality/config.php
@@ -4,4 +4,11 @@
    define('DB_PASSWORD', '');
    define('DB_DATABASE', 'mydb');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+/*
+DB_SERVER = address of the MySQL server
+DB_USERNAME = username used to connect to the database
+DB_PASSWORD = password for the database user
+DB_DATABASE = the name of the database to connect to
+*/
+
 ?>
