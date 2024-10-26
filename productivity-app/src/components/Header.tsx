b247fbd875aaad61cad4b913164c42d74f276637
Sunwoo Baek <62567659+sbaek21@users.noreply.github.com>
2024-10-26 01:18:06 -0500

    Updated frontend

--- productivity-app/src/components/Header.tsx
+++ productivity-app/src/components/Header.tsx
@@ -34,8 +34,6 @@
 //   );
 // };
 
-//웹사이트 위에 로고, TaskMaster << 서치바 선우님프사, "Sunwoo Baek" dropdown_menu 관련 html 파일
-
 // export default Header;
 import React from 'react';
 
@@ -52,7 +50,6 @@ const Header: React.FC<HeaderProps> = () => {
           </div>
           <img loading="lazy" src="src/assets/leftarrows.png" alt="" className="object-contain shrink-0 self-stretch my-auto aspect-[1.3] w-[26px]" />
         </div>
-        {/* 서치바, 아직 구현 안함 */}
         <form className="flex flex-col self-stretch my-auto text-sm rounded-none min-w-[240px] text-zinc-500 w-[417px]">
           <div className="flex gap-4 px-4 py-3 rounded-md bg-neutral-100">
             <img loading="lazy" src="src/assets/search.png" alt="" className="object-contain shrink-0 aspect-square w-[22px]" />
@@ -60,14 +57,11 @@ const Header: React.FC<HeaderProps> = () => {
           </div>
         </form>
       </div>
-
       <div className="flex gap-10 justify-between items-center my-auto text-base text-right text-slate-900">
         <div className="flex gap-4 items-center self-stretch my-auto">
-          {/* 선우님 프사랑 유저닉 */}
           <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/be23ea2b751a45ed7dea21be98e6d28c8e625b5714c5dc1b6c031f98bbaabcf4?placeholderIfAbsent=true&apiKey=4d7ed1808eb74d2f86f62f752e04c537" alt="Sunwoo Baek" className="object-contain shrink-0 self-stretch my-auto w-9 rounded-full aspect-square" />
           <div className="self-stretch my-auto">Sunwoo Baek</div>
         </div>
-        {/* 드랍다운 메뉴를 그냥 이미지만 가져다놓음.... */}
         <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/89b46925c5e6e9617f67cd7cbd29c93db1b5e229d573b46da0b3be365831cc1c?placeholderIfAbsent=true&apiKey=4d7ed1808eb74d2f86f62f752e04c537" alt="" className="object-contain shrink-0 self-stretch my-auto aspect-square w-[18px]" />
       </div>
     </header>
--- productivity-app/src/components/Sidebar.tsx
+++ productivity-app/src/components/Sidebar.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 const Sidebar: React.FC = () => {
   return (
-    // prob self-explanatory
     <nav className="flex flex-col items-start self-start mt-12 text-base font-medium whitespace-nowrap text-zinc-500 max-md:mt-10">
       <a href="#tasks" className="flex gap-3.5 items-center">
         <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/10ff5f0b06bd30e059d375ff33ef68aec17cf7a9d3c799aa56ed6d8df6bb4703?placeholderIfAbsent=true&apiKey=4d7ed1808eb74d2f86f62f752e04c537" alt="" className="object-contain shrink-0 self-stretch my-auto w-6 aspect-square" />
