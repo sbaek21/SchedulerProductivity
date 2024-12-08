a33d685800bd03426a0b0a6181c0c24388148f4d
choiyw109 <choiyw109@gmail.com>
2024-12-07 19:46:29 -0600

    commit

--- productivity-app/src/components/pages/Authentication/Login.tsx
+++ productivity-app/src/components/pages/Authentication/Login.tsx
@@ -3,16 +3,7 @@ import { loginUser, AuthenticationProps } from './api';
 import { useNavigate } from 'react-router-dom';
 import './Login.css';
 
-<<<<<<< HEAD
 const Login: React.FC<AuthenticationProps> = ({ setIsAuthenticated }) => {
-=======
-
-interface LoginProps {
-  setIsAuthenticated: (value: boolean) => void; // Accept setIsAuthenticated as a prop
-}
-
-const Login: React.FC<LoginProps> = ({ setIsAuthenticated }) => {
->>>>>>> f848ffe07844998d2b6f1ba3b1fe764bf83c38f3
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
@@ -23,13 +14,8 @@ const Login: React.FC<LoginProps> = ({ setIsAuthenticated }) => {
     try {
       const response = await loginUser(username, password);
       if (response.data.message === 'Login successful') {
-<<<<<<< HEAD
-        setIsAuthenticated(true)
-        navigate('/task');
-=======
         setIsAuthenticated(true); // Update authentication state
         navigate('/task'); // Navigate to the task page
->>>>>>> f848ffe07844998d2b6f1ba3b1fe764bf83c38f3
       }
     } catch (err: any) {
       console.error('Login error:', err);
@@ -44,7 +30,6 @@ const Login: React.FC<LoginProps> = ({ setIsAuthenticated }) => {
         <p className="login-subtitle">Log in to continue</p>
         <form onSubmit={handleSubmit}>
           <div className="input-container">
-<<<<<<< HEAD
           <i className="icon-user"></i>
           <input
                 type="text"
@@ -53,16 +38,6 @@ const Login: React.FC<LoginProps> = ({ setIsAuthenticated }) => {
                 onChange={(e) => setUsername(e.target.value)}
                 className="login-input"
               />
-=======
-            <i className="icon-user"></i>
-            <input
-              type="text"
-              placeholder="Username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              className="login-input"
-            />
->>>>>>> f848ffe07844998d2b6f1ba3b1fe764bf83c38f3
           </div>
           <div className="input-container">
             <i className="icon-lock"></i>
@@ -77,14 +52,10 @@ const Login: React.FC<LoginProps> = ({ setIsAuthenticated }) => {
           <button type="submit" className="login-button">
             Login
           </button>
-<<<<<<< HEAD
           </form>
-=======
-        </form>
->>>>>>> f848ffe07844998d2b6f1ba3b1fe764bf83c38f3
         {error && <p className="login-error">{error}</p>}
         <p className="login-footer">
-          Don’t have an account? <a href="/sign-up">Sign Up</a>
+          Don't have an account? <a href="/sign-up">Sign Up</a>
         </p>
       </div>
     </div>
